7b6eb4fe786ec6153429f731fbb556929ba22161
pinakim11 <pinakim11@gmail.com>
2017-04-21 13:55:04 -0400

    Initial Push

---
+++ spittr/src/main/java/com/example/web/HomeController.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.example.web;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author SR4PXM
+ *
+ */
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value="/index",method=GET)
+	public String home(){
+		return "home";
+	}
+
+}
